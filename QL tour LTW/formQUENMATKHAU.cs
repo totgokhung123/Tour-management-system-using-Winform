04a5dd576d76d0b15cd6220cae269558388a68f6
Chu Tiến Bình <141699568+totgokhung123@users.noreply.github.com>
2026-01-03 17:39:35 +0700

    fake .
    Updated email sender credentials in SendEmail method.

--- QL tour LTW/formQUENMATKHAU.cs
+++ QL tour LTW/formQUENMATKHAU.cs
@@ -43,8 +43,8 @@ namespace QL_tour_LTW
         }
         private void SendEmail(string toEmail, string subject, string body)
         {
-            string fromEmail = ""; // Thay thế bằng địa chỉ email của bạn
-            string password = ""; // Thay thế bằng mật khẩu B2 email của bạn
+            string fromEmail = "."; // Thay thế bằng địa chỉ email của bạn
+            string password = "."; // Thay thế bằng mật khẩu B2 email của bạn
 
             MailMessage mail = new MailMessage(fromEmail, toEmail, subject, body);
             SmtpClient client = new SmtpClient("smtp.gmail.com", 587);
